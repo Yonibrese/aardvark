f23e3eee9e24801c30d7b575fb78416cebe3da04
jonathan <yonibrese@gmail.com>
2020-04-26 23:46:35 +0300

    gallery view

--- gallery/views.py
+++ gallery/views.py
@@ -4,4 +4,4 @@ from . import models
 
 def all_products(request):
     products = models.Product.objects.all()
-    return render(request, 'gallery/all_products.html',{'products':products})
+    return render(request, 'gallery/all_products.html', {'products': products})
